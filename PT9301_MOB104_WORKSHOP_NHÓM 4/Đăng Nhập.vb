92a4a3ed150c5152595141ad7881608e28bb3144
CAPVN <tuandaps01899@fpt.edu.vn>
2015-07-25 15:55:45 +0700

    aaa

--- PT9301_MOB104_WORKSHOP_NHÓM 4/Đăng Nhập.vb
+++ PT9301_MOB104_WORKSHOP_NHÓM 4/Đăng Nhập.vb
@@ -2,9 +2,9 @@
 Public Class frm_dangnhap
 
     Private Sub btn_dangnhap_Click(sender As Object, e As EventArgs) Handles btn_dangnhap.Click
-        Dim chuoiketnoi As String = "workstation id=PT9301-NHOM4.mssql.somee.com;packet size=4096;user id=tuandaps01899_SQLLogin_1;pwd=t2qrbmfz1m;data source=PT9301-NHOM4.mssql.somee.com;persist security info=False;initial catalog=PT9301-NHOM4"
+        Dim chuoiketnoi As String = "workstation id=DB-NHOM4.mssql.somee.com;packet size=4096;user id=tuandaps01899_SQLLogin_1;pwd=24cft2ud9z;data source=DB-NHOM4.mssql.somee.com;persist security info=False;initial catalog=DB-NHOM4"
         Dim ketnoi As SqlConnection = New SqlConnection(chuoiketnoi)
-        Dim sqlAdapter As New SqlDataAdapter("select * from tai_khoan where user =' " & txt_user.Text & "' and pass='" & txt_pass.Text & "' ", ketnoi)
+        Dim sqlAdapter As New SqlDataAdapter("select * from tai_khoan where USER ='" & txt_user.Text & "'and PASS =' " & txt_pass.Text & "'", ketnoi)
         Dim tb As New DataTable
 
         Try
